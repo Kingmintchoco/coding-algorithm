6b69c9e0b05fa7f710476dc7382bcf75ee756a4e
Kingmintchoco <jrkr7@naver.com>
2022-05-22 17:11:13 +0900

    05 22

---
+++ chap43/p.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void init(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+}
+
+int main(){
+    init();
+}
